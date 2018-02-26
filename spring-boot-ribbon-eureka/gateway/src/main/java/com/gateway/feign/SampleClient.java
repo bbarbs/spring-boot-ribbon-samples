1a2df3616c03e0034be9c830bb55e7d9a6259cdb
bbarbs <barberobrainold@gmail.com>
2018-02-26 16:48:08 +0800

    Added sample implementation of eureka and ribbon

---
+++ spring-boot-ribbon-eureka/gateway/src/main/java/com/gateway/feign/SampleClient.java
@@ -0,0 +1,24 @@
+package com.gateway.feign;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+
+/**
+ * Customers rest api.
+ * <p>
+ * Note: It use the ribbon for client load balancer as added in yml.
+ */
+
+@FeignClient(
+        value = "customer-service"
+)
+public interface SampleClient {
+
+    @GetMapping(
+            value = "/test",
+            produces = TEXT_PLAIN_VALUE
+    )
+    String test();
+}
